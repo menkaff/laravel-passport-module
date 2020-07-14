7e52897ad0b6e327f74785d6fbcb84faaa03e23b
Mehran NikNafs <mehran.niknafs@gmail.com>
2020-07-14 12:53:18 +0430

    Remove unnecessary route

--- Routes/web.php
+++ Routes/web.php
@@ -9,8 +9,4 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
-Route::prefix('passport')->group(function() {
-    Route::get('/', 'PassportController@index');
-});
+ */
